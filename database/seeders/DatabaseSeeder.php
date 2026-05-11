d25ead522def32b5bf476b67c1abb8825ecc47b0
Omygod20 <2352620@csfoy.ca>
2026-05-11 13:32:44 -0400

    base project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Critic;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            LanguageSeeder::class,
+            FilmSeeder::class,
+            ActorSeeder::class,
+            FilmActorSeeder::class,
+            RoleSeeder::class,
+            AverageRatingSeeder::class,
+        ]);
+
+
+        //Ne sera pas fait dans le cadre de ce TP, les users et les critiques seront créés par vous
+        //User::factory(10)->has(Critic::factory(30))->create();
+    }
+}
